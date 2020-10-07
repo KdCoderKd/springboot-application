3673f7e56b396b087a1069e3503ad25337ca449e
KdCoderKd <72472692+KdCoderKd@users.noreply.github.com>
2020-10-07 12:01:41 +0100

    Update TaxonamyApplicationTest.java

--- taxonomy-spring-boot/src/test/java/com/embl/taxonamy/TaxonamyApplicationTest.java
+++ taxonomy-spring-boot/src/test/java/com/embl/taxonamy/TaxonamyApplicationTest.java
@@ -1,15 +1,23 @@
 package com.embl.taxonamy;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.embl.taxonomy.TaxonamyApplication;
+import com.embl.taxonomy.controller.StudyController;
 
 @SpringBootTest(classes = TaxonamyApplication.class)
 class TaxonamyApplicationTest {
 
+    @Autowired
+    private StudyController studyController;
+
     @Test
     void contextLoads() {
+        assertTrue(studyController != null);
     }
 
 }
